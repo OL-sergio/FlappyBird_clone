c3dfd9a68e3302108baabdc4fb47c6a2da6265c4
OL-sergio <a18074@alunos.ipca.pt>
2025-04-07 18:49:55 +0100

    Initial commit

---
+++ android/src/main/java/com/example/flappybird_clone/android/AndroidLauncher.java
@@ -0,0 +1,23 @@
+package com.example.flappybird_clone.android;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.PixelCopy;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.example.flappybird_clone.FlappyBirdActivity;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new FlappyBirdActivity(), configuration);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
+
+    }
+}
---
+++ core/src/main/java/com/example/flappybird_clone/FlappyBirdActivity.java
@@ -0,0 +1,36 @@
+package com.example.flappybird_clone;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class FlappyBirdActivity extends ApplicationAdapter {
+    private SpriteBatch batch;
+    private Texture image;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        image = new Texture("libgdx.png");
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        Gdx.gl.glClearColor(0.15f, 0.15f, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(image, 140, 210);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        image.dispose();
+    }
+}
