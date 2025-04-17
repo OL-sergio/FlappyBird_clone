47244fbe6242d6a436383e90f380191ecfb6a22e
Sérgio Oliveira <a18074@alunos.ipca.pt>
2025-04-17 16:26:44 +0100

    Work Done: - Adding adding the pipes movement and margin between the 2 two pipes. - Adding counter to count the number of times the bird passes the pipes and presented points to user.
    - Notes:
    - https://libgdx.com/wiki/start/project-generation
    - https://github.com/libgdx/gdx-liftoff/releases/tag/v1.13.1.3
    
    * Add items:
    -...
    
    * Updated items:
    - FlappyBirdActivity.java
    - build.gradle
    - libs.versions.toml
    
    * Removed:
    - ...

--- core/src/main/java/com/example/flappybird_clone/FlappyBirdActivity.java
+++ core/src/main/java/com/example/flappybird_clone/FlappyBirdActivity.java
@@ -2,79 +2,147 @@ package com.example.flappybird_clone;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import java.util.Random;
+
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class FlappyBirdActivity extends ApplicationAdapter {
 
-    private int movX = 0;
-    private int movY = 0;
 
     private SpriteBatch batch;
     private Texture[] birds;
     private Texture background;
+    private Texture pipeTop;
+    private Texture pipeTopLongo;
+    private Texture pipeBottom;
+    private Texture pipeBottomLongo;
+    private Random random;
+    private int score = 0;
+    private boolean passPipe = false;
+
 
     private float widthDisplay;
     private float heightDisplay;
-    private float wingsVariations = 0.0f;
-    private float gravity = 2.0f;
-    private float birdInitialVerticalPosition = 0.0f;
+    private float wingsVariations = 0f;
+    private float birdGravityJump = 2f;
+    private float birdInitialVerticalPosition;
+    private float pipePositionHorizontal;
+    private float pipePositionVertical ;
+    private float pipesMargin;
+
+    private BitmapFont textSoreResult;
+
 
 
     @Override
     public void create() {
-        //Gdx.app.log("", "Jogo iniciado");
-        batch = new SpriteBatch();
+        initializeTexture();
+        initializeObjects();
+    }
+
+    private void initializeTexture() {
 
         birds = new Texture[3];
         birds[0] = new Texture("passaro1.png");
         birds[1] = new Texture("passaro2.png");
         birds[2] = new Texture("passaro3.png");
-
         background = new Texture("fundo.png");
+
+        pipeTop = new Texture("cano_topo.png");
+        pipeTopLongo = new Texture("cano_topo_maior.png");
+
+        pipeBottom = new Texture("cano_baixo.png");
+        pipeBottomLongo = new Texture("cano_baixo_maior.png");
+
+    }
+
+    private void initializeObjects() {
+
+        //Gdx.app.log("", "Jogo iniciado");
+        random = new Random();
+        batch = new SpriteBatch();
         widthDisplay = Gdx.graphics.getWidth();
         heightDisplay = Gdx.graphics.getHeight();
-        birdInitialVerticalPosition = heightDisplay / 2;
+        birdInitialVerticalPosition = heightDisplay / 2f;
+        pipePositionHorizontal = widthDisplay;
+        pipesMargin = 260f;
+
+        textSoreResult = new BitmapFont();
+        textSoreResult.setColor(Color.WHITE);
+        textSoreResult.getData().setScale(6f);
 
     }
 
     @Override
     public void render() {
+        verifyEstateOfGame();
+        scoreValidation();
+        drawTexture();
+    }
 
-        /*
-        counter++;
-        Gdx.app.log("render", "Jogo rendering: " + counter);
-         */
+    private void scoreValidation() {
 
-        batch.begin();
+        if ( pipePositionHorizontal < 50f - birds[0].getWidth() ) {
+            if ( !passPipe ) {
+                score++;
+                passPipe = true;
+            }
+        }
+    }
 
-        if (wingsVariations > 3){
-            wingsVariations = 0;
+    private void verifyEstateOfGame() {
+
+        pipePositionHorizontal -= Gdx.graphics.getDeltaTime() * 200f;
+        if ( pipePositionHorizontal < -pipeTop.getWidth() ) {
+            pipePositionHorizontal = widthDisplay;
+            pipePositionVertical = random.nextInt(900 ) - 200f ;
+            passPipe = false;
         }
 
         boolean touchScreen = Gdx.input.isTouched();
         if ( touchScreen ) {
-           // Gdx.app.log("touch", "Tela tocada");
-            gravity = -25;
+            // Gdx.app.log("touch", "Tela tocada");
+            birdGravityJump = -15f;
         }
-            if ( birdInitialVerticalPosition > 0 || touchScreen ) {
-                birdInitialVerticalPosition = birdInitialVerticalPosition - gravity;
-            }
+        if ( birdInitialVerticalPosition > 0 || touchScreen ) {
+            birdInitialVerticalPosition = birdInitialVerticalPosition - birdGravityJump;
+        }
 
-            batch.draw( background, 0, 0, widthDisplay, heightDisplay );
-            batch.draw( birds[ (int) wingsVariations], 30, birdInitialVerticalPosition );
 
-            float flappingWingSpeed = Gdx.graphics.getDeltaTime() * 10;
+        wingsVariations += Gdx.graphics.getDeltaTime() * 10f;
+        if ( wingsVariations> 3){
+            wingsVariations = 0f;
+        }
 
-            wingsVariations += flappingWingSpeed;
-            //Gdx.app.log("variations", "variation: " +  );
-            //gravity++;
-            gravity  += 1.5f;
-            movY++;
-            movX++;
+        birdGravityJump++;
+    }
 
+    private void drawTexture() {
+
+
+        batch.begin();
+
+        batch.draw( background, 0f, 0f, widthDisplay, heightDisplay);
+        batch.draw( birds[ (int) wingsVariations], 50f, birdInitialVerticalPosition );
+
+        batch.draw(
+            pipeTop,
+            pipePositionHorizontal,
+            heightDisplay / 2f + pipesMargin / 2f + pipePositionVertical);
+
+        batch.draw(
+            pipeBottom,
+            pipePositionHorizontal,
+            heightDisplay / 2f - pipeBottom.getHeight() - pipesMargin/ 2f + pipePositionVertical);
+        textSoreResult.draw(batch, "Pontos: " + score , widthDisplay / 2f - 200f, heightDisplay - 200f
+        );
         batch.end();
+
     }
 
     @Override
