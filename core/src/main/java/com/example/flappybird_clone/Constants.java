1fc7edd58002fd9ef9e46176d2fdc42ee95722a8
OL-sergio <a18074@alunos.ipca.pt>
2025-05-02 17:24:37 +0100

    Work Done: - Adding a start menu to layer 0 of the game with logo, max score and message to start the game. - Adding image Logo and launcher new icons. - Adding launcher window background colour. - Updating the text score to start when the user touches the screen to start the game.
    - Notes:
    - https://libgdx.com/wiki/start/project-generation
    - https://github.com/libgdx/gdx-liftoff/releases/tag/v1.13.1.3
    
    * Add items:
    - flappy_bird.png
    - Adding the launcher icon.
    
    * Updated items:
    - FlappyBirdActivity.java
    - color.xml
    - AndroidManifest.xml
    - Constants.java
    - styles.xml
    
    * Removed:
    - ...

--- core/src/main/java/com/example/flappybird_clone/Constants.java
+++ core/src/main/java/com/example/flappybird_clone/Constants.java
@@ -1,18 +1,23 @@
 package com.example.flappybird_clone;
 
 public class Constants {
+
+
     private Constants() {
     }
 
     public static final int BIRD_START_WIDTH =  50;
-    public static final int PIPES_MARGIN =  500;
+    public static final int PIPES_MARGIN =  350;
     public static final int BIRD_JUMP_SIZE =  -10;
     public static final float BIRD_WINGS_SPEED =  10F;
-
-    public static final int PIPES_MAX_SIZE = 600;
-    public static final int PIPE_RANDOM_SIZE = 700;
+    public static final int PIPES_MAX_SIZE = 500;
+    public static final int PIPE_RANDOM_SIZE = 150;
     public static final int PIPES_WIDTH_SPEED = 300;
-
-
+    public static final String FLAPPY_BIRD_RECORD_POINTS = "flappyBird_recordPoints";
+    public static final String MAX_SCORE =  "maxScore";
+    public static final float BIRD_COLLISION_MOVEMENT = 500f ;
+    public static final float VIRTUAL_WIDTH = 720f;
+    public static final float VIRTUAL_HEIGHT = 1280f;
+    public static final float BIRD_START_POSITION = 200f;
 
 }
--- core/src/main/java/com/example/flappybird_clone/FlappyBirdActivity.java
+++ core/src/main/java/com/example/flappybird_clone/FlappyBirdActivity.java
@@ -1,11 +1,13 @@
 package com.example.flappybird_clone;
 
-import static com.example.flappybird_clone.Constants.PIPES_MAX_SIZE;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
@@ -14,6 +16,9 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 import java.util.PrimitiveIterator;
 import java.util.Random;
@@ -31,21 +36,24 @@ public class FlappyBirdActivity extends ApplicationAdapter {
     private Texture pipeBottom;
     private Texture pipeBottomLongo;
     private Texture gameOver;
+    private Texture imageTitle;
 
     private Random random;
     private int score = 0;
+    private int bestScore = 0;
     private int gameStatus = 0;
-    private float birdGravityHorizontal = 0f;
     private  boolean touchScreen;
     private boolean passPipe = false;
     private BitmapFont textScoreResult;
-    private BitmapFont textGameOverScoreResult;
+    private BitmapFont textGameOverRecordScore;
     private BitmapFont textGameRestart;
     private Sound soundFlying;
     private Sound soundCollision;
     private Sound soundPoint;
+    private Preferences preferences;
 
-
+    private OrthographicCamera camera;
+    private Viewport viewport;
     private ShapeRenderer shapeRenderer;
     private Circle birdCircle;
     private Rectangle pipeTopRectangle;
@@ -55,7 +63,8 @@ public class FlappyBirdActivity extends ApplicationAdapter {
     private float heightDisplay;
     private float wingsVariations = 0f;
     private float birdGravity = 2f;
-    private float birdInitialVerticalPosition;
+    private float birdInitialVerticalPosition = 0f;
+    private float birdInitialHorizontalPosition = 0f;
     private float pipePositionHorizontal;
     private float pipePositionVertical ;
     private float pipesMargin;
@@ -79,6 +88,7 @@ public class FlappyBirdActivity extends ApplicationAdapter {
         pipeBottom = new Texture("cano_baixo.png");
         pipeBottomLongo = new Texture("cano_baixo_maior.png");
         gameOver = new Texture("game_over.png");
+        imageTitle = new Texture("flappy_bird.png");
         soundFlying = Gdx.audio.newSound(Gdx.files.internal("som_asa.wav"));
         soundCollision = Gdx.audio.newSound(Gdx.files.internal("som_batida.wav"));
         soundPoint = Gdx.audio.newSound(Gdx.files.internal("som_pontos.wav"));
@@ -90,33 +100,48 @@ public class FlappyBirdActivity extends ApplicationAdapter {
         //Gdx.app.log("", "Jogo iniciado");
         random = new Random();
         batch = new SpriteBatch();
-        widthDisplay = Gdx.graphics.getWidth();
-        heightDisplay = Gdx.graphics.getHeight();
+        widthDisplay =  Constants.VIRTUAL_WIDTH;
+        heightDisplay = Constants.VIRTUAL_HEIGHT;
         birdInitialVerticalPosition = heightDisplay / 2f;
         pipePositionHorizontal = widthDisplay;
         pipesMargin = Constants.PIPES_MARGIN;
 
         textScoreResult = new BitmapFont();
         textScoreResult.setColor(Color.WHITE);
-        textScoreResult.getData().setScale(6f);
+        textScoreResult.getData().setScale(5f);
 
         textGameRestart = new BitmapFont();
         textGameRestart.setColor(Color.GREEN);
-        textGameRestart.getData().setScale(3f);
+        textGameRestart.getData().setScale(2f);
 
-        textGameOverScoreResult = new BitmapFont();
-        textGameOverScoreResult.setColor(Color.RED);
-        textGameOverScoreResult.getData().setScale(3f);
+        textGameOverRecordScore = new BitmapFont();
+        textGameOverRecordScore.setColor(Color.RED);
+        textGameOverRecordScore.getData().setScale(2f);
 
         shapeRenderer = new ShapeRenderer();
         birdCircle = new Circle();
         pipeTopRectangle = new Rectangle();
         pipeBottomRectangle = new Rectangle();
 
+        preferences = Gdx.app.getPreferences(Constants.FLAPPY_BIRD_RECORD_POINTS);
+        bestScore = preferences.getInteger(Constants.MAX_SCORE, 0);
+
+        camera = new OrthographicCamera();
+        camera.position.set(Constants.VIRTUAL_WIDTH / 2f, Constants.VIRTUAL_HEIGHT / 2f, 0);
+        viewport = new StretchViewport(Constants.VIRTUAL_WIDTH , Constants.VIRTUAL_HEIGHT, camera);
+
     }
 
     @Override
     public void render() {
+
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BITS);
+        Gdx.gl.glClear(Gdx.gl.GL_COLOR_BUFFER_BIT | Gdx.gl.GL_DEPTH_BUFFER_BIT);
+        int error = Gdx.gl.glGetError();
+        if (error != GL20.GL_NO_ERROR) {
+            Gdx.app.log("OpenGL Error", "Code: $error");
+        }
+
 
         verifyEstateOfGame();
         scoreValidation();
@@ -132,23 +157,23 @@ public class FlappyBirdActivity extends ApplicationAdapter {
 
         pipeTopRectangle.set(
             pipePositionHorizontal, heightDisplay / 2f + pipesMargin / 2f + pipePositionVertical,
-            pipeTop.getWidth(), pipeTop.getHeight()
+            pipeTopLongo.getWidth(), pipeTopLongo.getHeight()
         );
 
         pipeBottomRectangle.set(
             pipePositionHorizontal,
-            heightDisplay / 2f - pipeBottom.getHeight() - pipesMargin / 2f + pipePositionVertical,
-            pipeBottom.getWidth(), pipeBottom.getHeight()
+            heightDisplay / 2f - pipeBottomLongo.getHeight() / 2F - pipesMargin / 2f + pipePositionVertical,
+            pipeBottomLongo.getWidth(), pipeBottomLongo.getHeight()
         );
 
         boolean birdCollidedTop = Intersector.overlaps(birdCircle, pipeTopRectangle);
         boolean birdCollidedBottom = Intersector.overlaps(birdCircle, pipeBottomRectangle);
 
         if ( birdCollidedTop ||  birdCollidedBottom ) {
-            if( gameStatus == 1 ){
+            if( gameStatus == 2 ){
               // Gdx.app.log("Colisão", "Passaro colidiu com o cano");
                 soundCollision.play();
-                gameStatus = 2;
+                gameStatus = 3;
 
             }
 
@@ -198,12 +223,38 @@ public class FlappyBirdActivity extends ApplicationAdapter {
 
     private void verifyEstateOfGame() {
         touchScreen = Gdx.input.isTouched();
-        if ( gameStatus == 0 ) {
+
+        if (gameStatus == 0 ){
+            gameMenu();
+        } else if ( gameStatus == 1 ) {
             gameWaiting();
-        } else  if ( gameStatus == 1 ) {
+        } else  if ( gameStatus == 2 ) {
             gameStart();
-        } else if ( gameStatus == 2 ) {
+        } else if ( gameStatus == 3 ) {
             gameOverRestartGame();
+        }
+    }
+
+    private void gameMenu() {
+        if ( touchScreen ) {
+            // Gdx.app.log("touch", "Tela tocada");
+            gameStatus = 1;
+            soundFlying.play();
+        }
+
+        if ( birdInitialVerticalPosition > 0 || touchScreen ) {
+            birdInitialVerticalPosition = birdInitialVerticalPosition ;
+        }
+
+    }
+
+
+    private void gameWaiting() {
+        if ( touchScreen ) {
+            // Gdx.app.log("touch", "Tela tocada");
+            birdGravity= Constants.BIRD_JUMP_SIZE;
+            gameStatus = 2;
+            soundFlying.play();
         }
     }
 
@@ -211,14 +262,14 @@ public class FlappyBirdActivity extends ApplicationAdapter {
         if ( touchScreen ) {
             // Gdx.app.log("touch", "Tela tocada");
             birdGravity= Constants.BIRD_JUMP_SIZE;
-            gameStatus = 1;
+            gameStatus = 2;
             soundFlying.play();
         }
 
         pipePositionHorizontal -= Gdx.graphics.getDeltaTime() * Constants.PIPES_WIDTH_SPEED;
-        if ( pipePositionHorizontal < -pipeTop.getWidth() ) {
+        if ( pipePositionHorizontal <- pipeTopLongo.getWidth() ) {
             pipePositionHorizontal = widthDisplay;
-            pipePositionVertical = random.nextInt( Constants.PIPES_MAX_SIZE ) - Constants.PIPE_RANDOM_SIZE ;
+            pipePositionVertical = random.nextInt( Constants.PIPES_MAX_SIZE ) - random.nextInt(Constants.PIPE_RANDOM_SIZE)  ;
             Gdx.app.log("random", "Valor aleatorio: " + pipePositionVertical);
             passPipe = false;
 
@@ -227,31 +278,35 @@ public class FlappyBirdActivity extends ApplicationAdapter {
         if ( birdInitialVerticalPosition > 0 || touchScreen ) {
             birdInitialVerticalPosition = birdInitialVerticalPosition - birdGravity;
         }
-
         birdGravity++;
+
     }
 
-    private void gameWaiting() {
-        if ( touchScreen ) {
-            // Gdx.app.log("touch", "Tela tocada");
-            birdGravity= Constants.BIRD_JUMP_SIZE;
-            gameStatus = 1;
-            soundFlying.play();
-        }
-    }
+
 
     private void gameOverRestartGame() {
 
+      /*
         if ( birdInitialVerticalPosition > 0 || touchScreen ) {
             birdInitialVerticalPosition = birdInitialVerticalPosition - birdGravity;
-           // birdGravity++;
+            birdGravity++;
+        }
+        */
+        if (score > bestScore) {
+            bestScore = score;
+            preferences.putInteger(Constants.MAX_SCORE, bestScore);
+            preferences.flush();
+
         }
 
 
+        birdInitialHorizontalPosition -= Gdx.graphics.getDeltaTime() * Constants.BIRD_COLLISION_MOVEMENT;
+
         if ( touchScreen ) {
-            gameStatus = 0;
+            gameStatus = 1;
             score = 0;
             birdGravity = 0f;
+            birdInitialHorizontalPosition = 0f;
             birdInitialVerticalPosition = heightDisplay / 2f;
             pipePositionHorizontal = widthDisplay;
         }
@@ -259,40 +314,72 @@ public class FlappyBirdActivity extends ApplicationAdapter {
 
     private void drawTexture() {
 
+        batch.setProjectionMatrix( camera.combined );
 
         batch.begin();
 
         batch.draw( background, 0f, 0f, widthDisplay, heightDisplay);
-        batch.draw( birds[ (int) wingsVariations], 50f, birdInitialVerticalPosition );
+        batch.draw( birds[ (int) wingsVariations], 50f + birdInitialHorizontalPosition, birdInitialVerticalPosition - Constants.BIRD_START_POSITION );
 
         batch.draw(
-            pipeTop,
+            pipeTopLongo,
             pipePositionHorizontal,
             heightDisplay / 2f + pipesMargin / 2f + pipePositionVertical);
 
         batch.draw(
-            pipeBottom,
+            pipeBottomLongo,
             pipePositionHorizontal,
-            heightDisplay / 2f - pipeBottom.getHeight() - pipesMargin/ 2f + pipePositionVertical);
-        textScoreResult.draw(batch, "Pontos: " + score , widthDisplay / 2f - 200f, heightDisplay - 200f
-        );
+            heightDisplay / 2f - pipeBottomLongo.getHeight() - pipesMargin/ 2f + pipePositionVertical);
 
-        if ( gameStatus == 2 ){
+
+        if ( gameStatus == 0 ){
+
+            batch.draw(imageTitle, widthDisplay / 2f - gameOver.getWidth() / 2f,
+                heightDisplay / 1.8f - gameOver.getHeight() / 2f);
+
+            textGameRestart.draw(batch, "Toque para iniciar Jogo", widthDisplay / 1.85f - 200f,
+                heightDisplay / 1.50f - gameOver.getHeight() / 2f - 180f);
+
+            textGameOverRecordScore.draw(batch, "A melhor pontução: " + bestScore  + " pontos", widthDisplay / 1.84f - 200f,
+                heightDisplay /  1.8f - gameOver.getHeight() / 2f - 100f);
+
+        }
+
+        if ( gameStatus == 2){
+
+            textScoreResult.draw(
+                batch, "" + score , widthDisplay / 2f , heightDisplay - 200f
+            );
+
+        }
+
+        if ( gameStatus == 3 ){
+
+            textScoreResult.draw(
+                batch, "" + score , widthDisplay / 2f , heightDisplay - 200f
+            );
+
             batch.draw(gameOver, widthDisplay / 2f - gameOver.getWidth() / 2f,
-                heightDisplay / 2f - gameOver.getHeight() / 2f);
+                heightDisplay / 1.8f - gameOver.getHeight() / 2f);
 
-            textGameRestart.draw(batch, "Toque para reiniciar", widthDisplay / 2f - 200f,
-                heightDisplay / 2f - gameOver.getHeight() / 2f - 200f);
+            textGameRestart.draw(batch, "Toque para reiniciar", widthDisplay / 1.8f - 200f,
+                heightDisplay / 1.45f - gameOver.getHeight() / 2.2f - 210f);
 
-            textGameOverScoreResult.draw(batch, "A melhor pontução: " + score + " pontos", widthDisplay / 2f - 190f,
-                heightDisplay / 2f - gameOver.getHeight() / 2f - 100f);
+            textGameOverRecordScore.draw(batch, "A melhor pontução: " + bestScore  + " pontos", widthDisplay / 1.79f - 200f,
+                heightDisplay /  1.75f - gameOver.getHeight() / 2f - 110f);
         }
         batch.end();
 
     }
 
+    public void resize(int width, int height) {
+        viewport.update(width, height);
+        camera.update();
+    }
+
     @Override
     public void dispose() {
+        batch.dispose();
         Gdx.app.log("dispose", "Remover conteudos");
     }
 }
